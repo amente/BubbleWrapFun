74d248d63355999f6b6580be06dbf17aab1efc18
amente <amenteknows@gmail.com>
2013-01-22 11:15:06 -0500

    First commit, transfering all files from my local repo to github. App Basics is implemented. Popping and Sounds implemented. Scroll sound (has a bug, not a pleasant sound)

---
+++ BubbleWrap/src/com/example/bubblewrap/BubbleWrap.java
@@ -0,0 +1,47 @@
+package com.example.bubblewrap;
+
+public class BubbleWrap {
+
+	private int numberOfBubbles;
+	private int poppedBubbles;
+	private Integer[] bubbles;
+
+	public BubbleWrap(int numberOfBubbles, int bubble) {
+		this.numberOfBubbles = numberOfBubbles;
+		this.poppedBubbles = 0;
+		this.bubbles = new Integer[numberOfBubbles];
+		for (int i = 0; i < numberOfBubbles; i++) {
+			bubbles[i] = bubble;
+		}
+
+	}
+
+	public void popBubble(int index, int bubble) {
+
+		bubbles[index] = bubble;
+		poppedBubbles++;
+	}
+
+	public int getNumberOfPopped() {
+		return poppedBubbles;
+	}
+
+	public int getNumberOfBubbles() {
+		return numberOfBubbles;
+	}
+
+	public Integer[] getBubbles() {
+
+		return bubbles;
+
+	}
+
+	public boolean isBubble(int index, int bubble) {
+		if (bubbles[index] == bubble) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
