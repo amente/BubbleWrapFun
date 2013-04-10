19e7b4f3ff7184918973af08cd0ee8305e60a6df
Amente <amenteknows@gmail.com>
2013-04-10 06:22:40 -0400

    Added comments and a bit of refactoring

--- BubbleWrap/src/com/app/bubblewrap/BubbleGame.java
+++ BubbleWrap/src/com/app/bubblewrap/BubbleGame.java
@@ -18,59 +18,85 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.TextView;
 
+/*
+ * BubbleGame.java  is a the class the handles the gaming action (Popping Bubble Wraps) 
+ * 
+ * @author Amente Bekele
+ * @version 0.1
+ * 
+ */
 public class BubbleGame extends Application implements OnItemClickListener,
 		OnScrollListener, OnTouchListener {
 
-	public int SMALL_WRAP = 100;
+	// BubbleWrap sizes
+	public int SMALL_WRAP = 100; 
 	public int MEDIUM_WRAP = 500;
 	public int LARGE_WRAP = 1000;
 
-	public int DEFAULT_WRAP = SMALL_WRAP;
-	public int currentWrap = DEFAULT_WRAP;
+		
+	public int DEFAULT_WRAP = SMALL_WRAP; // The default size (Application starts)
+	public int currentWrap = DEFAULT_WRAP;//The currently selected size (Application restored from background) 
 
+	// Sound resources
 	private int[] POP_SOUNDS = { R.raw.pop_sound_1, R.raw.pop_sound_2,
 			R.raw.pop_sound_3, R.raw.pop_sound_4 };
-
 	private int DRAG_SOUND = R.raw.drag_sound;
 
+	//Image resources
 	private int UNPOPPED_BUBBLE = R.drawable.un_popped;
 	private int[] POPPED_BUBBLES = { R.drawable.popped_1, R.drawable.popped_2,
 			R.drawable.popped_3, R.drawable.popped_4 };
 
-	private Random random = new Random();
-	private SoundPool sound;
-	int[] popSounds;
-	int dragSound;
-	int dragSoundStream = 0;
-	private boolean scrollStarted = false;
-	Vibrator vibrator;
+	private Random random = new Random(); // Use for randomly selecting sound and popped images
+	private SoundPool sound; // Sound pool used for drag sound
+	int[] popSounds; // popSounds
+	int dragSound;  // drag Sounds
+	int dragSoundStream = 0; 
+	private boolean scrollStarted = false;// Is the view bieng scrolled
+	Vibrator vibrator; // Device vibrator reference
 
-	private int score;
-	private BubbleWrap bubbleWrap;
+	private int score; // Score (Number of popped bubbles
+	private BubbleWrap bubbleWrap; // BubbleWrap reference
 
 	// UI Elements
-	private MainActivity activity;
-	private TextView scoreText;
-	private GridView bubbleGrid;
-	// private TextView numberOfBubblesText;
+	private MainActivity activity; // Main App activity
+	private TextView scoreText; 
+	private GridView bubbleGrid; 
+	
 
-	private ImageAdapter adapter;
+	private ImageAdapter adapter;// Image adapter used for rendering and updating images 
 
+	/* 
+	 * Gets the current score  
+	 * @return score
+	 */
 	public int getScore() {
 		return bubbleWrap.getNumberOfBubbles() - score;
 	}
 
+	/*
+	 * Gets the bubble wrap
+	 * @return bubbleWrap
+	 */
 	public BubbleWrap getBubbleWrap() {
 		return bubbleWrap;
 	}
 
 	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Application#onCreate()
+	 */
 	public void onCreate() {
 		super.onCreate();
 		bubbleWrap = new BubbleWrap(DEFAULT_WRAP, UNPOPPED_BUBBLE);
 
 	}
-
+     
+	/*
+	 * (non-Javadoc)
+	 * @see android.widget.AdapterView.OnItemClickListener#onItemClick(android.widget.AdapterView, android.view.View, int, long)
+	 */
 	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
 
 		vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
@@ -89,21 +115,26 @@ public class BubbleGame extends Application implements OnItemClickListener,
 
 	}
 
+	/*
+	 * Initialises the main activity (Renders views)
+	 * 
+	 */
 	public void initializeUi(MainActivity activity) {
+		//Get the activity
 		this.activity = activity;
 
+		//New image adapter, for the bubbles from the Wrap's GridView
 		adapter = new ImageAdapter(activity, bubbleWrap.getBubbles());
 
+		// Set the score text and font
 		scoreText = (TextView) activity.findViewById(R.id.poppedNumber);
 		scoreText.setText(this.getScore() + "");
 		Typeface lcdFont = Typeface.createFromAsset(getAssets(),
 				"fonts/LCDM2N__.TTF");
 		scoreText.setTypeface(lcdFont);
 
-		// numberOfBubblesText = (TextView) activity
-		// .findViewById(R.id.unPoppedNumber);
-		// numberOfBubblesText.setText(bubbleWrap.getNumberOfBubbles() + "");
-
+		
+		// Get the grid and attach listeners
 		bubbleGrid = (GridView) activity.findViewById(R.id.gridview);
 		bubbleGrid.setAdapter(adapter);
 		bubbleGrid.setOnItemClickListener(this);
@@ -111,29 +142,41 @@ public class BubbleGame extends Application implements OnItemClickListener,
 		bubbleGrid.setOnTouchListener(this);
 		bubbleGrid.setBackgroundResource(R.color.back_ground);
 
+		// Create the sound pool
 		sound = new SoundPool(POP_SOUNDS.length + 1, AudioManager.STREAM_MUSIC,
 				0);
 		popSounds = new int[POP_SOUNDS.length];
 
+		// Keep reference of the sounds
 		for (int i = 0; i < popSounds.length; i++) {
 			popSounds[i] = sound.load(activity, POP_SOUNDS[i], 1);
 		}
 
+		//Keep reference for the drag sound (Currently Buggy)
 		dragSound = sound.load(activity, DRAG_SOUND, 1);
 
-		// bubbleGrid.setOnTouchListener(this);
-
+		
 	}
 
+	/*
+	 * Updates the the user interface 
+	 */
 	public void updateUi() {
 
+		//Notify the adapter to replace images
 		adapter.notifyDataSetChanged();
+		//Update the score
 		scoreText.setText(this.getScore() + "");
-		// numberOfBubblesText.setText(bubbleWrap.getNumberOfBubbles() + "");
+		//Check all bubbles are popped
 		checkDone();
 
 	}
 
+	/*
+	 * Creates a new game
+	 * 
+	 * @param size the size of the bubble wrap
+	 */
 	public void newGame(int size) {
 		bubbleWrap = new BubbleWrap(size, UNPOPPED_BUBBLE);
 		score = bubbleWrap.getNumberOfPopped();
@@ -142,7 +185,11 @@ public class BubbleGame extends Application implements OnItemClickListener,
 		updateUi();
 
 	}
-
+	
+	
+    /*
+     * Checks if the game is over
+     */
 	public void checkDone() {
 		if (this.getScore() == 0) {
 			scoreText.setText(R.string.done_text);
@@ -150,6 +197,10 @@ public class BubbleGame extends Application implements OnItemClickListener,
 	}
 
 	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see android.widget.AbsListView.OnScrollListener#onScroll(android.widget.AbsListView, int, int, int)
+	 */
 	public void onScroll(AbsListView arg0, int arg1, int arg2, int arg3) {
 		// TODO Auto-generated method stub
 		dragSoundStream = 0;
@@ -157,6 +208,10 @@ public class BubbleGame extends Application implements OnItemClickListener,
 	}
 
 	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see android.widget.AbsListView.OnScrollListener#onScrollStateChanged(android.widget.AbsListView, int)
+	 */
 	public void onScrollStateChanged(AbsListView arg0, int arg1) {
 		// TODO Auto-generated method stub
 		scrollStarted = true;
@@ -164,6 +219,10 @@ public class BubbleGame extends Application implements OnItemClickListener,
 	}
 
 	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see android.view.View.OnTouchListener#onTouch(android.view.View, android.view.MotionEvent)
+	 */
 	public boolean onTouch(View v, MotionEvent event) {
 		if (event.getAction() == MotionEvent.ACTION_MOVE) {
 
--- BubbleWrap/src/com/app/bubblewrap/BubbleWrap.java
+++ BubbleWrap/src/com/app/bubblewrap/BubbleWrap.java
@@ -1,11 +1,23 @@
 package com.app.bubblewrap;
 
+/*
+ * Represents a BubbleWrap object. It keeps track of the number of poppedBubbles * 
+ * 
+ * @author Amente Bekele
+ * @version 0.1
+ */
 public class BubbleWrap {
 
 	private int numberOfBubbles;
 	private int poppedBubbles;
 	private Integer[] bubbles;
 
+	/*
+	 *Constructs a new BubbleWrap
+	 *
+	 *@param numberOfBubbles the number of bubbles
+	 *@param bubble the resource id of unpopped bubble image
+	 */
 	public BubbleWrap(int numberOfBubbles, int bubble) {
 		this.numberOfBubbles = numberOfBubbles;
 		this.poppedBubbles = 0;
@@ -15,27 +27,48 @@ public class BubbleWrap {
 		}
 
 	}
-
+    
+	/*
+	 * Pops a bubble
+	 * @param index the index of the bubble
+	 * @param resource id of the image to replace with
+	 */
 	public void popBubble(int index, int bubble) {
 
 		bubbles[index] = bubble;
 		poppedBubbles++;
 	}
 
+	/*
+	 * @return the number of popped bubbles
+	 */
 	public int getNumberOfPopped() {
 		return poppedBubbles;
 	}
 
+	/*
+	 * @return the number of bubbles
+	 */
 	public int getNumberOfBubbles() {
 		return numberOfBubbles;
 	}
 
+	/*
+	 * @return the bubbles
+	 */
 	public Integer[] getBubbles() {
 
 		return bubbles;
 
 	}
 
+	/*
+	 * 
+	 * @param the index of a bubble
+	 * @param the resource id of the image used
+	 * 
+	 * @return true if the bubble is not popped 
+	 */
 	public boolean isBubble(int index, int bubble) {
 		if (bubbles[index] == bubble) {
 			return true;
